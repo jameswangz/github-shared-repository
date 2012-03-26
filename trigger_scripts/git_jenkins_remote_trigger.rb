da8c1717dcfadd2064834682faffb59bbfeba543
James Wang <jameswangz81@gmail.com>
2012-03-26 15:01:17 +0800

    Added trigger scripts

---
+++ trigger_scripts/git_jenkins_remote_trigger.rb
@@ -0,0 +1,81 @@
+require 'net/http'
+require 'uri'
+
+##
+# This script is used to trigger jenkins jobs remotely based on a 'shared' git reposiotry, to make it works you must put this file
+# in the respository's root folder, as far as we know, there are 2 ways to create a shared git repository
+#
+#   1. Configure the workspaceDir in JENKINS_HOME/config.xml, set the value to the git repository folder 
+#   2. Create soft links for all jobs, source ->  git repository folder, target -> job/workspace 
+# 
+# We may create a Jenkins plugin later but it works currently. 
+# 
+# @author James Wang (james.wang.z81@gmail.com)
+# @since Feb 22, 2012
+#
+##
+class GitJenkinsRemoteTrigger
+	
+	def initialize(
+		jenkins, 
+		module_job_mappings, 
+		running_options = { :only_once => true }, 
+		auth_options = { :required => false },
+		other_options = { :COMMIT_ID_PARAM_NAME => 'GIT_COMMIT_ID'	}
+	)	
+		@jenkins = jenkins
+		@module_job_mappings = module_job_mappings
+		@running_options = running_options
+		@auth_options = auth_options		
+		@other_options = other_options
+	end
+
+	def run
+		if @running_options[:only_once]
+			run_once
+		else	
+			while true
+				run_once
+				sleep @running_options[:interval] 
+			end
+		end
+	end
+
+	def run_once
+		pull_result = %x[git pull origin master]
+		puts pull_result
+		return if pull_result.include? 'Already up-to-date'
+		if pull_result.empty?
+			puts 'Error! Couldn\'t pull from git repository,  please check your network setting and SSH key.'
+			return
+		end
+		@module_job_mappings.each do |module_name, job_name|
+			result = %x[git log --quiet HEAD~..HEAD #{module_name}]
+			puts "Result of #{module_name} [#{result}]"
+			if not result.empty?
+				result =~ /commit\s+(.+)/
+				puts "commit id #{$1}"
+				trigger job_name, $1 
+			end
+		end
+	end
+
+	def trigger(job_name, commit_id)
+		puts "triggering job #{job_name}"
+		uri = URI("#{@jenkins}/job/#{job_name}/buildWithParameters?#{@other_options[:COMMIT_ID_PARAM_NAME]}=#{commit_id}")			
+		puts uri
+		begin
+			if @auth_options[:required] 
+				req = Net::HTTP::Get.new(uri.request_uri)
+				req.basic_auth @auth_options[:username], @auth_options[:api_token] 
+				res = Net::HTTP.start(uri.host, uri.port) { |http| http.request(req) }
+				puts res.body
+			else
+				Net::HTTP.get_print uri 
+			end
+		rescue => e
+			puts "Trigger error!!! -> #{e}"
+		end
+	end
+
+end
